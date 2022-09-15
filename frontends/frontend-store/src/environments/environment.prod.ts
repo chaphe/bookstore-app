ff2e560e7b89e78c8c1a56d81c768a47cd1fb468
Gabriel Pedraza <gabo_pedraza@yahoo.com>
2022-09-15 15:42:34 -0500

    Se elimina el directorio del frontend de store para incorporar el nuevo código

--- frontends/frontend-store/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  storeUrl:
-    window['env']['storeUrl'] || 'http://localhost:8082',
-  reviewsUrl:
-    window['env']['reviewsUrl'] || 'http://localhost:3000',
-  catalogUrl:
-    window['env']['catalogUrl'] || 'http://localhost:8081'
-};
